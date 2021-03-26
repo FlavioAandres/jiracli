53f5cde3be17e3942c33aa50f640393f3b9f75ab
FlavioAandres <fpareja@condorlabs.io>
2021-03-26 12:11:04 -0500

    Look for all reporters or current User

--- src/commands/findIssue.js
+++ src/commands/findIssue.js
@@ -4,9 +4,6 @@ const Table = require('cli-table3');
 const { Input, Select } = require('enquirer')
 const urlHelper = require('../helpers/url')
 
-// hola si sonido uno dos tres, 4, 0 
-
-
 class FindIssueCommand extends Command {
   InputObject = (message = "", initial = "abc") => new Input({
     message,
@@ -24,7 +21,12 @@ class FindIssueCommand extends Command {
     if(!flags.name){
       flags.name = await this.InputObject("Add a name to search", "Query in prod").run()
     }
-    const JQL = `summary ~ "${flags.name}" AND reporter in (currentUser()) order by created DESC`
+
+    const JQL = `summary ~ "${flags.name}" 
+      ${flags.owner ? 'AND reporter in (currentUser())' : ''}
+      order by created DESC
+    `
+    console.log(JQL)
     const JQLResult = await JiraInstance.searchIssue(JQL)
     
     const issuesTable = new Table({
@@ -49,7 +51,8 @@ Extra documentation goes here
 `
 
 FindIssueCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
+  name: flags.string({char: 'n', description: 'name to print.'}),
+  owner: flags.boolean({char: 'o', description: 'Look for the issues reported by me.'})
 }
 
 module.exports = FindIssueCommand
